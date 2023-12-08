d5d167a857912bcad40d519dff0debcb76647326
rooney <bch3454@gmail.com>
2023-12-08 14:29:17 +0900

    Study: 이진 탐색 - 이진 탐색으로 정렬된 배열에서 특정 원소의 개수 구하기

---
+++ concept/이진 탐색 - 정렬된 배열에서 특정 원소의 개수 구하기/index.js
@@ -0,0 +1,17 @@
+function lowerBound(arr, target, start, end) {
+  while (start < end) {
+    let mid = parseInt((start + end) / 2);
+    if (arr[mid] >= target) end = mid;
+    else start = mid + 1; // 최대한 왼쪽으로 이동하기
+  }
+  return end;
+}
+
+function upperBound(arr, target, start, end) {
+  while (start < end) {
+    let mid = parseInt((start + end) / 2);
+    if (arr[mid] > target) end = mid;
+    else start = mid + 1; // 최대한 오른쪽으로 이동하기
+  }
+  return end;
+}
