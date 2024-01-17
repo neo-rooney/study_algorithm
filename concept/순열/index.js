1262f9ff4a0e3710cd6f3695bad4feec8eff8c80
rooney <bch3454@gmail.com>
2024-01-17 18:22:54 +0900

    Study: js로 순열 구현

--- concept/순열/index.js
+++ concept/순열/index.js
@@ -1,6 +1,20 @@
-const fs = require("fs");
-const filePath =
-  process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
-const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const getPermutation = (arr, select) => {
+  const resultArray = [];
 
+  if (select === 1) {
+    return arr.map((item) => [item]);
+  }
 
+  arr.map((fixed, index, origin) => {
+    const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
+    const permutations = getPermutation(rest, select - 1);
+    const result = permutations.map((item) => [fixed, ...item]);
+    resultArray.push(...result);
+  });
+
+  return resultArray;
+};
+
+const test = getPermutation([1, 2, 3, 4], 2);
+
+console.log(test);
